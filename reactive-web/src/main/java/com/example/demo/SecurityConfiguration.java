5a2ec973bd1e72548b46c98de53074a296c82ec4
Matt Raible <matt.raible@okta.com>
2018-09-23 08:05:34 -0600

    Fix Security Configuration

--- reactive-web/src/main/java/com/example/demo/SecurityConfiguration.java
+++ reactive-web/src/main/java/com/example/demo/SecurityConfiguration.java
@@ -1,40 +1,49 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoders;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 import java.util.Collections;
 
 @Configuration
-@EnableWebSecurity
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+@EnableWebFluxSecurity
+@EnableReactiveMethodSecurity
+public class SecurityConfiguration {
+    private final String issuerUri;
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-            .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
-                .and()
-            .csrf()
-                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-                .and()
+    public SecurityConfiguration(@Value("${spring.security.oauth2.client.provider.okta.issuer-uri}") String issuerUri) {
+        this.issuerUri = issuerUri;
+    }
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        return http
+            .csrf().disable()
             .cors()
                 .and()
-            .authorizeRequests()
-                .anyRequest().authenticated()
+            .authorizeExchange()
+                .anyExchange().authenticated()
                 .and()
             .oauth2Login()
                 .and()
             .oauth2ResourceServer()
-                .jwt();
+                .jwt()
+            .and().and().build();
+    }
+
+    @Bean
+    ReactiveJwtDecoder jwtDecoder() {
+        return ReactiveJwtDecoders.fromOidcIssuerLocation(this.issuerUri);
     }
 
     @Bean
