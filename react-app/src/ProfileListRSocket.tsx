41e907d7effeb8836b657f13532ac645050a0647
Matt Raible <matt.raible@okta.com>
2018-11-18 13:33:59 -0700

    Polishing

--- react-app/src/ProfileListRSocket.tsx
+++ react-app/src/ProfileListRSocket.tsx
@@ -5,17 +5,21 @@ import {
   JsonSerializers,
 } from 'rsocket-core';
 import RSocketWebSocketClient from 'rsocket-websocket-client';
+
 interface Profile {
   id: number;
   email: string;
 }
+
 interface ProfileListProps {
   auth: Auth;
 }
+
 interface ProfileListState {
   profiles: Array<Profile>;
   isLoading: boolean;
 }
+
 // Create an instance of a client
 const client = new RSocketClient({
   // send/receive objects instead of strings/buffers
@@ -32,14 +36,18 @@ const client = new RSocketClient({
   },
   transport: new RSocketWebSocketClient({url: 'ws://localhost:8080/ws/profiles'}),
 });
+
 class ProfileListRSocket extends React.Component<ProfileListProps, ProfileListState> {
+
   constructor(props: ProfileListProps) {
     super(props);
+
     this.state = {
       profiles: [],
       isLoading: false
     };
   }
+
   async componentDidMount() {
     client.connect().subscribe({
       onComplete: (socket: any) => {
@@ -50,6 +58,7 @@ class ProfileListRSocket extends React.Component<ProfileListProps, ProfileListSt
         console.log('onNext(%s)', payload.data);
       },
     });
+
     this.setState({isLoading: true});
     const response = await fetch('http://localhost:8080/profiles', {
       headers: {
@@ -59,11 +68,14 @@ class ProfileListRSocket extends React.Component<ProfileListProps, ProfileListSt
     const data = await response.json();
     this.setState({profiles: data, isLoading: false});
   }
+
   render() {
     const {profiles, isLoading} = this.state;
+
     if (isLoading) {
       return <p>Loading...</p>;
     }
+
     return (
       <div>
         <h2>Profile List</h2>
@@ -76,4 +88,5 @@ class ProfileListRSocket extends React.Component<ProfileListProps, ProfileListSt
     );
   }
 }
+
 export default ProfileListRSocket;
