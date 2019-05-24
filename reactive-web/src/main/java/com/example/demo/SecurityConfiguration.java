3587917410d1f9754a3604ede6f71e1cc9a33f76
Matt Raible <matt@raibledesigns.com>
2019-05-24 09:19:08 -0600

    Update to use Spring Boot 2.1.5 (#13)

--- reactive-web/src/main/java/com/example/demo/SecurityConfiguration.java
+++ reactive-web/src/main/java/com/example/demo/SecurityConfiguration.java
@@ -1,14 +1,11 @@
 package com.example.demo;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.security.web.server.csrf.CookieServerCsrfTokenRepository;
 
-@EnableWebFluxSecurity 
+@EnableWebFluxSecurity
 public class SecurityConfiguration {
 
     @Bean
